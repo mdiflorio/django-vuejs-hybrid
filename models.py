550b64f15e6e3049a1806c9e2dbbefed94c4e478
Madhava Di Florio <mattdiflorio@gmail.com>
2020-10-04 13:49:38 +0200

    Add __str__ to models

--- models.py
+++ models.py
@@ -5,13 +5,19 @@ from django.db import models
 
 
 class Musician(models.Model):
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
 
 
 class Album(models.Model):
+    def __str__(self):
+        return "{0} - {1}".format(self.name, self.release_date)
+
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
-    num_stars = models.IntegerField()
+    num_stars = models.IntegerField(verbose_name="Number of stars")
