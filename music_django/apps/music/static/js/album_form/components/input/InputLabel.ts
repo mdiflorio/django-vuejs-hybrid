d88e83294f823508784ececb86d7bbe4cfb40e2d
Madhava Di Florio <mattdiflorio@gmail.com>
2020-10-20 20:07:20 +0200

    Clean up code

--- music_django/apps/music/static/js/album_form/components/input/InputLabel.ts
+++ music_django/apps/music/static/js/album_form/components/input/InputLabel.ts
@@ -5,9 +5,8 @@ import { Component, Prop } from 'vue-property-decorator';
     name: "InputLabel",
     template: `
         <label class="block text-gray-700 text-sm font-bold mb-2">
-            {{ label }}
-            <span v-if="required">*</span></label
-        >
+            {{ label }} <span v-if="required">*</span>
+        </label>
     `
 })
 export default class InputLabel extends Vue {
--- music_django/apps/music/static/js/album_form/components/input/templates/standard.ts
+++ music_django/apps/music/static/js/album_form/components/input/templates/standard.ts
@@ -6,7 +6,12 @@ export const standard = `
                 :type="fieldType"
                 v-model="binding"
                 :required="field.required"
-                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2  rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                
+                class="block appearance-none w-full bg-white 
+                border border-gray-400 hover:border-gray-500 
+                px-4 py-2  rounded shadow leading-tight 
+                focus:outline-none focus:shadow-outline"
+                
                 :class="{ 'border-red-500': errors }"
             />
             <InputErrors :errors="errors" />
